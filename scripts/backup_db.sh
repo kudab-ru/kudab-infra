fd3bc51951a001ee35f41ddc50f8964ae07a31e5
Solomka-0 <krasjuk2@gmail.com>
2025-07-20 02:55:09 +0300

    feature(infra): Базовая инфраструктура

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+mkdir -p backups
 docker-compose exec -T kudab-db pg_dump -U kudab kudab > backups/db_backup_$TIMESTAMP.sql
+
+# Удаляем все, кроме 5 последних дампов
+ls -1t backups/db_backup_*.sql | tail -n +6 | xargs -r rm --
