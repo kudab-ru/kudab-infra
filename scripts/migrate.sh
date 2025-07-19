af691aecbbed48bea146da711156e6d5ebfd2969
Solomka-0 <krasjuk2@gmail.com>
2025-07-20 02:40:38 +0300

    feature(infra): Базовая инфраструктура

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker-compose exec kudab-api php artisan migrate --force
-docker-compose exec kudab-parser php artisan migrate --force
+docker-compose exec -T kudab-api php artisan migrate --force
+docker-compose exec -T kudab-parser php artisan migrate --force
